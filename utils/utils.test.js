4da2900314963dbb00b2e875519f84179a66265e
muhammad yousuf <muhammadyousuf327@gmail.com>
2019-10-07 16:18:49 +0500

    number square test

--- utils/utils.test.js
+++ utils/utils.test.js
@@ -1,8 +1,15 @@
-const utils = require('./utlis');
+const utils = require("./utlis");
 
-it('should add two numbers', ()=>{
-    let res = utils.add(5, 22);
-    if(res !== 27){
-        throw new Error(`Expected 27 but result is ${res}`)
-    }
-})
+it("should add two numbers", () => {
+  let res = utils.add(5, 22);
+  if (res !== 27) {
+    throw new Error(`Expected 27 but result is ${res}`);
+  }
+});
+
+it("should return square value", () => {
+  let res = utils.square(5);
+  if (res !== 25) {
+    throw new Error(`Expected 25 but result is ${res}`);
+  }
+});
