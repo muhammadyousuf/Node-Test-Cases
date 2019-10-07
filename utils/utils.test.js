1c65b799779c71a97eaaf9d58666debd44660993
muhammad yousuf <muhammadyousuf327@gmail.com>
2019-10-07 16:55:57 +0500

    create three more test case

--- utils/utils.test.js
+++ utils/utils.test.js
@@ -1,10 +1,9 @@
+const expect = require("expect");
 const utils = require("./utlis");
 
 it("should add two numbers", () => {
   let res = utils.add(5, 22);
-  if (res !== 27) {
-    throw new Error(`Expected 27 but result is ${res}`);
-  }
+  expect(res).toBe(27);
 });
 
 it("should return square value", () => {
@@ -13,3 +12,16 @@ it("should return square value", () => {
     throw new Error(`Expected 25 but result is ${res}`);
   }
 });
+
+it("should have equal to number", () => {
+  expect(27).toBe(27);
+  expect(typeof 27).toBe('number');
+});
+
+it("should have not equal to number", () => {
+    expect(27).not.toBe(57);
+  });
+
+it("should have equal to objec", () => {
+    expect({name:'Yosuf'}).toEqual({name:'Yosuf'});
+  });
