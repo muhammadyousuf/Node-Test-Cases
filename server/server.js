6b1608c7e8030664bef20ced2b1bac6a640df584
muhammad yousuf <muhammadyousuf327@gmail.com>
2019-10-12 23:29:41 +0500

    create basic express server

---
+++ server/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+let app = express();
+let port = 5000
+app.get('/', (req, res) => {
+    res.send('Hello world')
+})
+
+app.listen(port, () => {
+    console.log(`server is running on port ${port}`);
+})
