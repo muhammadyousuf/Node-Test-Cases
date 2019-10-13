13f21af90ea2e3f780b1c6e01eb45c86ebfa20f0
muhammad yousuf <muhammadyousuf327@gmail.com>
2019-10-13 13:26:22 +0500

    test users list api

--- server/server.js
+++ server/server.js
@@ -3,9 +3,25 @@ const express = require('express');
 let app = express();
 let port = 5000
 app.get('/', (req, res) => {
-    res.send('Hello world')
+    res.status(404).send({ error: 'Page not found!' })
 })
 
+app.get('/users', (req, res) => {
+    res.status(200).send([
+        {
+            name: 'yousuf',
+            age: 24
+        },
+        {
+            name: 'rafae',
+            age: 26
+        },
+        {
+            name: 'raza',
+            age: 25
+        }
+    ])
+})
 app.listen(port, () => {
     console.log(`server is running on port ${port}`);
 })
--- server/server.test.js
+++ server/server.test.js
@@ -1,9 +1,33 @@
 const request = require('supertest');
+const expect = require('expect');
 let app = require('./server').app;
 
-it('should have response Hello World', (done)=>{
+it('should have response Hello World', (done) => {
     request(app)
         .get('/')
-        .expect('Hello world')
+        .expect(404)
+        .expect((res) => {
+            expect(res.body).toMatchObject(
+                {
+                    error: 'Page not found!'
+                }
+            )
+        })
+        .end(done)
+})
+
+
+it('should have response user object', (done) => {
+    request(app)
+        .get('/users')
+        .expect(200)
+        .expect((res) => {
+            expect(res.body[0]).toMatchObject(
+                {
+                    name: 'yousuf',
+                    age: 24
+                }
+            )
+        })
         .end(done)
 })
